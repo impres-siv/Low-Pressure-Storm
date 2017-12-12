5302faa4b3dfdc4868bfff8968a76ba899baf3c7
impres-siv <33914442+impres-siv@users.noreply.github.com>
2017-12-12 15:41:02 -0500

    lil better

--- twythonDemo.py
+++ twythonDemo.py
@@ -5,11 +5,11 @@ twitter = Twython(app_key,app_secret,oauth_token,oauth_token_secret)
 
 results = twitter.search(q="#vanguardhackathon2017", count=3)
 all_tweets = results['statuses']
-
+userNames=[]
 for tweet in all_tweets:
-	userName = tweet['user']['screen_name']
+	userNames.append(tweet['user']['screen_name'])
 	id = str(tweet['id'])
 
-
-statushead = "@" + userName + " "
-twitter.update_status(status=statushead+"Take our retirement quiz to find out how you can save! goo.gl/jCT21d", in_reply_to_status_id=id)
+for userName in userNames
+	statushead = "@" + userName + " "
+	twitter.update_status(status=statushead+"Take our retirement quiz to find out how you can save! goo.gl/jCT21d", in_reply_to_status_id=id)
